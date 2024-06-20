3d2da8d3030b963c0e3c6e653ddc4fe6ecd98122
koomin1227 <koomin1227@naver.com>
2024-06-20 17:47:08 +0900

    M3-125 Feat : 각 화면에 해당하는 임시 appBar 구현

---
+++ lib/widgets/app_bar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class MapAppBar extends StatelessWidget {
+  const MapAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      title: const Text('지도'),
+    );
+  }
+}
+
+class RankingAppBar extends StatelessWidget {
+  const RankingAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      title: const Text('랭킹'),
+    );
+  }
+}
+
+class GroupAppBar extends StatelessWidget {
+  const GroupAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      title: const Text('그룹'),
+    );
+  }
+}
+
+class MyPageAppBar extends StatelessWidget {
+  const MyPageAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      title: const Text('마이페이지'),
+    );
+  }
+}
