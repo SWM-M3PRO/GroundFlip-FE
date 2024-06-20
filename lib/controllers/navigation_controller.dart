ae7d9cf2dbff1d9800ccf759d8d79ad92fe44e69
koomin1227 <koomin1227@naver.com>
2024-06-20 17:34:25 +0900

    M3-125 Feat : NavigationController 탭 했을때 띄어질 페이지 추가

--- lib/controllers/navigation_controller.dart
+++ lib/controllers/navigation_controller.dart
@@ -1,9 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+
+import '../screens/group_screen.dart';
+import '../screens/map_screen.dart';
+import '../screens/my_page_screen.dart';
+import '../screens/ranking_screen.dart';
 
 class NavigationController extends GetxController {
   final RxInt selectedIndex = 0.obs;
+  static List<Widget> tabPages = <Widget>[
+    const MapScreen(),
+    const RankigScreen(),
+    const GroupScreen(),
+    const MyPageScreen(),
+  ];
 
   void changeIndex(int index) {
     selectedIndex(index);
+  }
+
+  Widget getCurrentPage() {
+    return tabPages[selectedIndex.value];
   }
 }
