803a1062d0c84323541de69849f9ce108151ceb1
koomin1227 <koomin1227@naver.com>
2024-06-20 17:34:49 +0900

    M3-125 Feat : 임시 화면 구현 (지도, 랭킹, 그룹, 마이페이지)

---
+++ lib/screens/ranking_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class RankigScreen extends StatelessWidget {
+  const RankigScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text('랭킹'),
+      ],
+    );
+  }
+}
