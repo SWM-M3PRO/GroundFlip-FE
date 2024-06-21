6ea85ce4b7397063805906e3952c9db35f238aef
cha-imag <100341870+tkdals802@users.noreply.github.com>
2024-06-21 18:13:55 +0900

    M3-107 Feat : develop branch rebase

--- lib/utils/walking_service.dart
+++ lib/utils/walking_service.dart
@@ -1,6 +1,8 @@
 abstract class WalkingService {
+
   Future<int?> getCurrentStep();
 
+  List<int> getWeeklySteps(startDate, endDate);
   Future<List<int>> getDailyStepsInInterval(
     DateTime startDate,
     DateTime endDate,
