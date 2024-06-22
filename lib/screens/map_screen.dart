22462d25e0efc7095252c7f9bc7c3a03b3613413
cha-imag <100341870+tkdals802@users.noreply.github.com>
2024-06-22 23:57:25 +0900

    M3-107 Feat : 안드로이드 만보기 일정 주기 초기화되도록 구현

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+import '../service/android_walk_interface.dart';
 
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
 
+
+
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Text('지도'),
-      ],
-    );
+    final AndroidWalkInterface AndroidWalkController =
+      Get.put(AndroidWalkInterface());
+    return Column(
+        children: [
+          Text('지도'),
+          Obx(() => Text('${AndroidWalkController.steps.value}'))
+          ],
+      );
   }
 }
