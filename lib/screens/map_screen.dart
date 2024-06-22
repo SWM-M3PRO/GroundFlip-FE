2f63cc233c81428a76cfe7b288a3da1d611a66f9
qjvk2880 <qjvk2880@gmail.com>
2024-06-22 12:00:31 +0900

    M3-114 Refactor : 미사용 변수 제거

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -10,7 +10,6 @@ class MapScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final PermissionController permissionController =
     Get.put(PermissionController());
     final MapController mapController = Get.put(MapController());
 
