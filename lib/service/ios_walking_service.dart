356dde04ce429e74487a3faa73f1499ee1c0152f
koomin1227 <koomin1227@naver.com>
2024-06-26 23:41:31 +0900

    M3-113 Feat : 걸음수를 가져올때 null 이면 0으로 변경

--- lib/service/ios_walking_service.dart
+++ lib/service/ios_walking_service.dart
@@ -37,10 +37,9 @@ class IosWalkingService implements WalkingService {
         date = date.add(const Duration(days: 1))) {
       DateTime startOfDay = DateTime(date.year, date.month, date.day, 0, 0, 0);
       DateTime endOfDay = DateTime(date.year, date.month, date.day, 23, 59, 59);
-      int? dailyStep =
-          await Health().getTotalStepsInInterval(startOfDay, endOfDay);
-
-      dailySteps.add(dailyStep!);
+      int dailyStep =
+          await Health().getTotalStepsInInterval(startOfDay, endOfDay) ?? 0;
+      dailySteps.add(dailyStep);
     }
 
     return dailySteps;
