77959f6d3be808009806cad07a432c3f0ef05d3e
koomin1227 <koomin1227@naver.com>
2024-06-27 00:53:23 +0900

    M3-113 Feat : mock AndroidWalkingService 구현

---
+++ lib/service/android_walking_service.dart
@@ -0,0 +1,15 @@
+import '../utils/walking_service.dart';
+
+//ToDo : 안드로이드 서비스 실제로 구현하기
+class AndroidWalkingService implements WalkingService {
+  @override
+  Future<int> getCurrentStep() {
+    return Future.value(4251);
+  }
+
+  @override
+  Future<List<int>> getDailyStepsInInterval(
+      DateTime startDate, DateTime endDate) {
+    return Future.value([1500, 2500, 3500, 4500, 5500, 6500, 7500]);
+  }
+}
