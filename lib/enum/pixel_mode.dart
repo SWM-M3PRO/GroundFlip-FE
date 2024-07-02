3edba2aa408bdbb85337c67088f6c55a7887c718
MinUk <qjvk2880@gmail.com>
2024-07-02 12:47:04 +0900

    M3-116 Feat : 모드의 한글 이름으로부터 enum을 얻는 함수 추가

--- lib/enum/pixel_mode.dart
+++ lib/enum/pixel_mode.dart
@@ -1,9 +1,14 @@
 enum PixelMode {
-  individualMode(name : '개인전'),
-  individualHistory(name : '개인 기록'),
-  groupMode(name : '그룹전');
+  individualMode(krName : '개인전'),
+  individualHistory(krName : '개인 기록'),
+  groupMode(krName : '그룹전');
 
-  const PixelMode({required this.name});
+  const PixelMode({required this.krName});
 
-  final String name;
+  final String krName;
+
+  static PixelMode fromKrName(String krName) {
+    return PixelMode.values.firstWhere((mode) => mode.krName == krName,
+        orElse: () => throw ArgumentError('No enum value with krName $krName'));
+  }
 }
