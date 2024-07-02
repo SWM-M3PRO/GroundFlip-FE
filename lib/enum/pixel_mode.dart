1c6b29a98e64e6d6f68972834943f22e5c1bc60c
MinUk <qjvk2880@gmail.com>
2024-07-02 12:37:13 +0900

    M3-116 Refactor : 모드를 나타내는 enum에 name 필드 추가

--- lib/enum/pixel_mode.dart
+++ lib/enum/pixel_mode.dart
@@ -1,5 +1,9 @@
 enum PixelMode {
-  individualMode,
-  individualHistory,
-  groupMode
+  individualMode(name : '개인전'),
+  individualHistory(name : '개인 기록'),
+  groupMode(name : '그룹전');
+
+  const PixelMode({required this.name});
+
+  final String name;
 }
