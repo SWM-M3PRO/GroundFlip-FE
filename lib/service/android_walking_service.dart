eef8724c833265f2faa0f28c95dd71b50877acee
koomin1227 <koomin1227@naver.com>
2024-07-03 20:18:29 +0900

    M3-112 Feat : android_walking_service.dart 수정

--- lib/service/android_walking_service.dart
+++ lib/service/android_walking_service.dart
@@ -1,10 +1,77 @@
+import 'dart:async';
+
+import 'package:get_storage/get_storage.dart';
+import 'package:pedometer/pedometer.dart';
+
 import '../utils/walking_service.dart';
 
-//ToDo : 안드로이드 서비스 실제로 구현하기
 class AndroidWalkingService implements WalkingService {
+  final GetStorage localStorage = GetStorage();
+
+  static String LOCAL_STORE_STEP_KEY = 'pastSteps';
+  static int checkDay = 0;
+
+  late Stream<StepCount> _stepCountStream;
+
+  int currentSteps = 0;
+  int totalSteps = 0;
+  int pastSteps = 0;
+
+  static final AndroidWalkingService _instance =
+      AndroidWalkingService._internal();
+
+  AndroidWalkingService._internal() {
+    print('생성');
+    initPlatformState();
+    init();
+    Timer.periodic(Duration(minutes: 5), (t) {
+      if (checkDay != DateTime.now().day) {
+        resetStepTimer();
+        checkDay = DateTime.now().day;
+      }
+    });
+  }
+
+  factory AndroidWalkingService() {
+    return _instance;
+  }
+
+  void init() async {
+    await GetStorage.init();
+  }
+
+  void initPlatformState() {
+    _stepCountStream = Pedometer.stepCountStream.asBroadcastStream();
+    _stepCountStream.listen(updateStep).onError(onStepCountError);
+  }
+
+  void onStepCountError(error) {
+    currentSteps = 0;
+  }
+
+  void updateStep(StepCount event) async {
+    totalSteps = event.steps;
+
+    int? value = localStorage.read(LOCAL_STORE_STEP_KEY);
+    if (value == null || value == 0) {
+      pastSteps = totalSteps;
+      localStorage.write(LOCAL_STORE_STEP_KEY, totalSteps);
+    } else {
+      pastSteps = value;
+    }
+    currentSteps = totalSteps - pastSteps;
+    print(currentSteps);
+  }
+
+  void resetStepTimer() async {
+    pastSteps = totalSteps;
+    currentSteps = 0;
+    localStorage.write(LOCAL_STORE_STEP_KEY, totalSteps);
+  }
+
   @override
   Future<int> getCurrentStep() {
-    return Future.value(4251);
+    return Future.value(currentSteps);
   }
 
   @override
