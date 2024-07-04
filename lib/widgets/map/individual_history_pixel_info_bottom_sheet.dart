68e2b28f53ae5c96164d9dbea2c4a3633f5aaee9
MinUk <qjvk2880@gmail.com>
2024-07-04 17:24:15 +0900

    M3-146 Feat : 개인 기록 픽셀 정보 ui 구현 완료

--- lib/widgets/map/individual_history_pixel_info_bottom_sheet.dart
+++ lib/widgets/map/individual_history_pixel_info_bottom_sheet.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
 import '../../models/individual_history_pixel_info.dart';
+import 'individual_visit_history_list_view.dart';
 
 class IndividualHistoryPixelInfoBottomSheet extends StatelessWidget {
-  const IndividualHistoryPixelInfoBottomSheet({super.key, required this.pixelInfo});
+  const IndividualHistoryPixelInfoBottomSheet(
+      {super.key, required this.pixelInfo});
 
   final IndividualHistoryPixelInfo pixelInfo;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,10 +19,41 @@ class IndividualHistoryPixelInfoBottomSheet extends StatelessWidget {
           topLeft: Radius.circular(20),
         ),
       ),
-
       child: SizedBox(
         height: 400,
         width: 380,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  '${pixelInfo.address ?? '대한민국'} ${pixelInfo.addressNumber ?? 'n'}번째 픽셀',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  '${pixelInfo.visitCount}번 방문했어요!',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              IndividualVisitHistoryListView(visitList: pixelInfo.visitList!),
+            ],
+          ),
+        ),
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,15 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+import 'package:ground_flip/models/individual_history_pixel_info.dart';
+import 'package:ground_flip/service/pixel_service.dart';
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+  test('test', () async {
+    final service = PixelService();
+    IndividualHistoryPixelInfo pixel = await service.getIndividualHistoryPixelInfo(pixelId: 4049087, userId: 2);
+    print(pixel.visitList);
   });
 }
