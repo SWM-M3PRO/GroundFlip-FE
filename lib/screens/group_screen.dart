bedf2965c35cc3ec6c4f3ea20b36cb7fd9d84d3e
cha-imag <100341870+tkdals802@users.noreply.github.com>
2024-07-06 13:45:12 +0900

    M3-107 Refactor : code refact

--- lib/screens/group_screen.dart
+++ lib/screens/group_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'search_group_screen.dart';
+import '../service/pixel_service.dart';
 
 class GroupScreen extends StatelessWidget {
   const GroupScreen({super.key});
