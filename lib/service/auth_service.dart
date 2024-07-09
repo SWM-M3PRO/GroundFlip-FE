7de0fc2b9e3c2f81b918daf179153b2d9558e349
koomin1227 <koomin1227@naver.com>
2024-07-09 15:30:17 +0900

    M3-159 Feat : 카카오 서버에서 인증하고 access token을 획득하는 기능 구현

---
+++ lib/service/auth_service.dart
@@ -0,0 +1,54 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/services.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+
+import '../utils/dio_service.dart';
+
+class AuthService {
+  static final AuthService _instance = AuthService._internal();
+
+  final Dio dio = DioService().getDio();
+
+  AuthService._internal();
+
+  factory AuthService() {
+    return _instance;
+  }
+
+  Future<String> getKakaoAccessToken() async {
+    OAuthToken? token;
+    if (await isKakaoTalkInstalled()) {
+      token = await _loginWithKakaoTalkApp();
+    } else {
+      token = await _loginWithKakaoAccount();
+    }
+    return token.accessToken;
+  }
+
+  Future<OAuthToken> _loginWithKakaoTalkApp() async {
+    OAuthToken? token;
+    try {
+      token = await UserApi.instance.loginWithKakaoTalk();
+      print('카카오톡으로 로그인 성공 ${token.accessToken}');
+    } catch (error) {
+      print('카카오톡으로 로그인 실패 $error');
+
+      if (error is PlatformException && error.code == 'CANCELED') {
+        throw Exception("로그인 실패");
+      }
+      token = await _loginWithKakaoAccount();
+    }
+    return token;
+  }
+
+  Future<OAuthToken> _loginWithKakaoAccount() async {
+    try {
+      OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
+      print('카카오계정으로 로그인 성공 ${token.accessToken}');
+      return token;
+    } catch (error) {
+      print('카카오계정으로 로그인 실패 $error');
+      throw Exception("로그인 실패");
+    }
+  }
+}
