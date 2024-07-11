ab802885fda5bc7e689496d554a44b1b85d138b4
koomin1227 <koomin1227@naver.com>
2024-07-11 14:51:36 +0900

    M3-181 Feat : 로그아웃 버튼을 눌렀을때 로그아웃 할 건지 물어보는 모달 창 생성

--- lib/controllers/setting_controller.dart
+++ lib/controllers/setting_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../service/auth_service.dart';
@@ -6,7 +7,30 @@ class SettingController extends GetxController {
   final AuthService authService = AuthService();
 
   logout() async {
-    await authService.logout();
-    Get.offAllNamed('/login');
+    _showLogoutDialog();
+  }
+
+  void _showLogoutDialog() {
+    Get.dialog(
+      AlertDialog(
+        title: Text('로그아웃 하시겠습니까?'),
+        actions: [
+          TextButton(
+            child: Text('아니오'),
+            onPressed: () async {
+              await authService.logout();
+              Get.back();
+            },
+          ),
+          TextButton(
+            child: Text('예'),
+            onPressed: () async {
+              await authService.logout();
+              Get.offAllNamed('/login');
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
