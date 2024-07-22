155d50347402a7f13571b0860a102c853b9769c1
Koo Min <koomin1227@naver.com>
2024-07-22 19:14:14 +0900

    M3-205 Feat : 소셜 로그인 버튼 디자인 적용

--- lib/constants/social_login_button_style.dart
+++ lib/constants/social_login_button_style.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+
 import '../utils/social_login_button_style.dart';
 
 class SocialLoginButtonStyles {
   static SocialLoginButtonStyle apple = SocialLoginButtonStyle(
-    Colors.black,
-    "assets/images/apple_logo.png",
-    "Apple로 로그인",
     Colors.white,
+    "assets/images/apple_logo.png",
+    "Apple로 시작하기",
+    Colors.black,
   );
 
   static SocialLoginButtonStyle kakao = SocialLoginButtonStyle(
     Color(0xFFFEE500),
     "assets/images/kakao_logo.png",
-    "카카오로 로그인",
+    "카카오로 시작하기",
     Colors.black,
   );
 }
