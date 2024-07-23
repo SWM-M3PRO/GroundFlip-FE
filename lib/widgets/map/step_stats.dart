a32c02a461b7729532b66c78ae5e68ae99a5415c
Koo Min <koomin1227@naver.com>
2024-07-23 22:22:17 +0900

    M3-203 Feat : 걸음수 정보 디자인 적용

--- lib/widgets/map/step_stats.dart
+++ lib/widgets/map/step_stats.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 
-import '../../constants/app_colors.dart';
 import '../../constants/text_styles.dart';
 import '../../controllers/walking_controller.dart';
 
@@ -13,30 +11,21 @@ class StepStats extends StatelessWidget {
   Widget build(BuildContext context) {
     var walkController = Get.find<WalkingController>();
     return Obx(
-      () => Column(
+      () => Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           Text(
-            '${NumberFormat('###,###,###').format(walkController.getCurrentStep())} 걸음',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
+            walkController.getCurrentStep(),
+            style: TextStyles.fs24w900cTextPrimary,
           ),
-          SizedBox(
-            height: 5,
+          Text(
+            ' 걸음',
+            style: TextStyles.fs17w400cTextSecondary,
           ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 7, vertical: 5),
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-            child: Text(
-              '${NumberFormat('###,###,###').format(walkController.getCurrentCalorie())} kcal · ${NumberFormat('###,###,###').format(walkController.getCurrentTravelDistance())}km',
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-            ),
+          Spacer(),
+          Text(
+            '${walkController.getCurrentCalorie()} kcal · ${walkController.getCurrentTravelDistance()}km',
+            style: TextStyles.fs17w400cTextSecondary,
           ),
         ],
       ),
@@ -44,78 +33,16 @@ class StepStats extends StatelessWidget {
   }
 }
 
-class MapBottomSheet extends StatelessWidget {
-  const MapBottomSheet({super.key});
+// Todo: 추후에 대시보드 등의 형식으로 변경 필요
+class StepStatsBody extends StatelessWidget {
+  const StepStatsBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var walkController = Get.find<WalkingController>();
-    return DraggableScrollableSheet(
-      controller: walkController.draggableController,
-      snap: true,
-      initialChildSize: 0.11,
-      minChildSize: 0.11,
-      maxChildSize: 0.6,
-      builder: (BuildContext context, scrollController) {
-        return Container(
-          clipBehavior: Clip.hardEdge,
-          decoration: BoxDecoration(
-            color: AppColors.backgroundSecondary,
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(32),
-              topRight: Radius.circular(32),
-            ),
-          ),
-          child: CustomScrollView(
-            controller: scrollController,
-            slivers: [
-              SliverToBoxAdapter(
-                child: Center(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: AppColors.backgroundThird,
-                      borderRadius: const BorderRadius.all(Radius.circular(10)),
-                    ),
-                    height: 4,
-                    width: 40,
-                    margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  ),
-                ),
-              ),
-              SliverToBoxAdapter(
-                child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Obx(
-                      () => Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text(
-                            walkController.getCurrentStep(),
-                            style: TextStyles.fs24w900cTextPrimary,
-                          ),
-                          Text(
-                            ' 걸음',
-                            style: TextStyles.fs17w400cTextSecondary,
-                          ),
-                          Spacer(),
-                          Text(
-                            '${walkController.getCurrentCalorie()} kcal · ${walkController.getCurrentTravelDistance()}km',
-                            style: TextStyles.fs17w400cTextSecondary,
-                          ),
-                        ],
-                      ),
-                    )),
-              ),
-              SliverList.list(
-                children: const [
-                  ListTile(title: Text('Jane Doe')),
-                  ListTile(title: Text('Jack Reacher')),
-                ],
-              ),
-            ],
-          ),
-        );
-      },
+    return SliverToBoxAdapter(
+      child: SizedBox(
+        width: 10,
+      ),
     );
   }
 }
