2c78e7482979dbce5bb54da4608f3f952e7d63b0
Koo Min <koomin1227@naver.com>
2024-07-23 13:19:47 +0900

    M3-203 Feat : 네비게이션 바에 구분선 추가

--- lib/widgets/common/naviagtion_bar.dart
+++ lib/widgets/common/naviagtion_bar.dart
@@ -10,22 +10,29 @@ class CustomBottomNavigationBar extends GetView<NavigationController> {
   @override
   Widget build(BuildContext context) {
     return Obx(
-      () => BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: controller.selectedIndex.value,
-        onTap: controller.changeIndex,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.map), label: "지도"),
-          BottomNavigationBarItem(icon: Icon(Icons.leaderboard), label: "랭킹"),
-          // BottomNavigationBarItem(icon: Icon(Icons.group), label: "그룹"),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: "마이",
+      () => Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(color: AppColors.backgroundThird, width: 0.5),
           ),
-        ],
-        selectedItemColor: AppColors.primary,
-        unselectedItemColor: AppColors.textSecondary,
-        backgroundColor: AppColors.navigationBarColor,
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: controller.selectedIndex.value,
+          onTap: controller.changeIndex,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.map), label: "지도"),
+            BottomNavigationBarItem(icon: Icon(Icons.leaderboard), label: "랭킹"),
+            // BottomNavigationBarItem(icon: Icon(Icons.group), label: "그룹"),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: "마이",
+            ),
+          ],
+          selectedItemColor: AppColors.primary,
+          unselectedItemColor: AppColors.textSecondary,
+          backgroundColor: AppColors.navigationBarColor,
+        ),
       ),
     );
   }
