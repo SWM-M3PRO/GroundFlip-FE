c26213d8de247ab321e12fc883f8924f97de927c
MinUk <qjvk2880@gmail.com>
2024-07-23 21:00:05 +0900

    M3-208 Feat : 애플 권한 동의 구현 완료

--- lib/screens/permission_request_screen.dart
+++ lib/screens/permission_request_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -34,8 +36,8 @@ class PermissionRequestScreen extends StatelessWidget {
                         width: 20.0,
                       ),
                       Text(
-                          '서비스 사용을 위해 \n'
-                          '아래의 권한이 필요합니다.',
+                        '서비스 사용을 위해 \n'
+                        '아래의 권한이 필요합니다.',
                         style: TextStyles.fs28w700cTextPrimary,
                       ),
                     ],
@@ -44,29 +46,45 @@ class PermissionRequestScreen extends StatelessWidget {
                     height: 20.0,
                   ),
                   PermissionElement(
-                    iconPath: 'assets/images/permission/location_permission_icon.png',
+                    iconPath:
+                        'assets/images/permission/location_permission_icon.png',
                     permissionName: '위치 정보',
                     permissionDescription: '지도에서 위치를 표시하기 위해 필요해요!',
                   ),
                   SizedBox(
                     height: 16.0,
                   ),
-                  PermissionElement(
-                    iconPath: 'assets/images/permission/activity_recognition_icon.png',
-                    permissionName: '신체 활동',
-                    permissionDescription: '걸음 수를 가져오기 위해 필요해요!',
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  PermissionElement(
-                    iconPath: 'assets/images/permission/notification_icon.png',
-                    permissionName: '알림',
-                    permissionDescription: '걸음 수를 알림창에 표시하기 위해 필요해요!',
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
+                  if (Platform.isAndroid) ...[
+                    PermissionElement(
+                      iconPath:
+                          'assets/images/permission/activity_recognition_icon.png',
+                      permissionName: '신체 활동',
+                      permissionDescription: '걸음 수를 가져오기 위해 필요해요!',
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    PermissionElement(
+                      iconPath:
+                          'assets/images/permission/notification_icon.png',
+                      permissionName: '알림',
+                      permissionDescription: '걸음 수를 알림창에 표시하기 위해 필요해요!',
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                  ],
+                  if (Platform.isIOS) ...[
+                    PermissionElement(
+                      iconPath:
+                          'assets/images/permission/activity_recognition_icon.png',
+                      permissionName: '애플 건강',
+                      permissionDescription: '걸음 수를 가져오기 위해 필요해요!',
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                  ],
                 ],
               ),
             ),
