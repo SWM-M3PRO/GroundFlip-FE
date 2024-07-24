e0173be23a5dad8a44c6836d96ac7dfe63452fea
cha-imag <100341870+tkdals802@users.noreply.github.com>
2024-07-24 15:33:08 +0900

    M3-191 Feat : dart fix

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../models/auth_response.dart';
-import '../screens/policy_screen.dart';
 import '../service/auth_service.dart';
 
 class LoginController extends GetxController {
