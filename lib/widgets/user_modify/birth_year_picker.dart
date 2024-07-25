471246a1e01a76897efca935264cd9940262f4ac
cha-imag <100341870+tkdals802@users.noreply.github.com>
2024-07-25 14:09:37 +0900

    M3-204 Feat : birth year picker

---
+++ lib/widgets/user_modify/birth_year_picker.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../constants/app_colors.dart';
+import '../../constants/text_styles.dart';
+import '../../controllers/user_info_controller.dart';
+
+class BirthYearPicker extends StatelessWidget {
+  BirthYearPicker({super.key});
+
+  int selectedNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<int> yearOptions =
+        List.generate(2024 - 1900 + 1, (index) => 1900 + index);
+
+    final UserInfoController userInfoController =
+        Get.find<UserInfoController>();
+    final FixedExtentScrollController controller = FixedExtentScrollController(
+      initialItem: userInfoController.birthYear.value - 1900,
+    );
+    selectedNumber = userInfoController.birthYear.value;
+    return SizedBox(
+      width: 600,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    "출생연도 선택",
+                    style: TextStyles.fs20w700cTextPrimary,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.back();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.backgroundThird,
+                    shape: CircleBorder(),
+                    minimumSize: Size(36, 36),
+                    padding: EdgeInsets.all(0),
+                    iconColor: AppColors.textPrimary,
+                  ),
+                  child: Icon(Icons.close),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: GestureDetector(
+                onTap: () {
+                  userInfoController.selectBirthYear(selectedNumber);
+                  Get.back();
+                },
+                child: CupertinoPicker(
+                  itemExtent: 60,
+                  magnification: 1.22,
+                  selectionOverlay: CupertinoPickerDefaultSelectionOverlay(
+                    background: AppColors.secondary,
+                  ),
+                  onSelectedItemChanged: (selectedYear) {
+                    selectedNumber = yearOptions[selectedYear];
+                    userInfoController.birthYear.value = yearOptions[selectedYear];
+                  },
+                  scrollController: controller,
+                  children: yearOptions.map((year) {
+                    return Center(
+                      child: Text(
+                        year.toString(),
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                          color: AppColors.textPrimary,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
