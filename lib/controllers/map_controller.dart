f5e875afbc59d3b335df57a90c63c3ea721004c1
MinUk <qjvk2880@gmail.com>
2024-07-31 21:46:41 +0900

    M3-250 Refactor : 커스텀 마커를 위한 메소드 제거

--- lib/controllers/map_controller.dart
+++ lib/controllers/map_controller.dart
@@ -299,18 +299,6 @@ class MapController extends SuperController {
     }
   }
 
-  Future<Uint8List> getBytesFromAsset(String path, int width) async {
-    ByteData data = await rootBundle.load(path);
-    ui.Codec codec = await ui.instantiateImageCodec(
-      data.buffer.asUint8List(),
-      targetWidth: width,
-    );
-    ui.FrameInfo fi = await codec.getNextFrame();
-    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
-        .buffer
-        .asUint8List();
-  }
-
   _convertSpeedToKmPerHour(double? speed) {
     if (speed == null) {
       return -1;
