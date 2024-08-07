dca934634240fb851f26097dda4a0f95f86b8572
Koo Min <koomin1227@naver.com>
2024-08-07 22:03:52 +0900

    M3-285 Feat : 메인페이지에 진입하면 토큰 등록

--- lib/controllers/main_controller.dart
+++ lib/controllers/main_controller.dart
@@ -1,8 +1,13 @@
 import 'package:get/get.dart';
 
+import '../service/fcm_service.dart';
+
 class MainController extends GetxController {
+  final FcmService fcmService = FcmService();
+
   @override
   void onInit() async {
     super.onInit();
+    fcmService.registerFcmToken();
   }
 }
