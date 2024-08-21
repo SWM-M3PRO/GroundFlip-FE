12c64c5db2c88371963c05a098120532a3760c48
Koo Min <koomin1227@naver.com>
2024-08-21 13:02:51 +0900

    M3-295 Feat : 권한 동의 메서드 구현

--- lib/service/permission_service.dart
+++ lib/service/permission_service.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 
 import '../models/permission.dart';
 import '../utils/dio_service.dart';
+import '../utils/user_manager.dart';
 
 class PermissionService {
   static final PermissionService _instance = PermissionService._internal();
@@ -14,8 +15,31 @@ class PermissionService {
     return _instance;
   }
 
-  Future<Permission> getPermission(int userId) async {
+  Future<Permission> getPermission() async {
+    int? userId = UserManager().getUserId();
     var response = await dio.get('/permission/$userId');
     return Permission.fromJson(response.data['data']);
+  }
+
+  void updateServiceNotification(bool isEnabled) {
+    int? userId = UserManager().getUserId();
+    dio.put(
+      '/permission/service-notification',
+      data: {
+        "userId": userId,
+        "isEnabled": isEnabled,
+      },
+    );
+  }
+
+  void updateMarketingNotification(bool isEnabled) {
+    int? userId = UserManager().getUserId();
+    dio.put(
+      '/permission/marketing-notification',
+      data: {
+        "userId": userId,
+        "isEnabled": isEnabled,
+      },
+    );
   }
 }
