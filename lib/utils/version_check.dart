ce9022861931f131bf881eda6e4107150dcd5d0e
cha-imag <100341870+tkdals802@users.noreply.github.com>
2024-08-21 14:54:50 +0900

    M3-317 Feat : 버전 업그레이드

--- lib/utils/version_check.dart
+++ lib/utils/version_check.dart
@@ -15,9 +15,67 @@ class VersionCheck {
       "https://apps.apple.com/app/ground-flip/id6550922550";
   final VersionService versionService = VersionService();
 
-  Future<dynamic> versionCheck() async{
+  Future<dynamic> versionCheck() async {
     VersionResponse versionResponse = await versionService.getVersion();
     if (versionResponse.needUpdate == "NEED") {
+      return Get.dialog(
+        AlertDialog(
+          title: Text(
+            "앱의 최신버전이 존재합니다.",
+            style: TextStyle(
+              color: AppColors.textPrimary,
+              fontSize: 20,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          actions: [
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 0),
+                child: Row(
+                  children: [
+                    TextButton(
+                      child: Text(
+                        '확인',
+                        style: TextStyle(
+                          color: AppColors.primary,
+                          fontSize: 17,
+                        ),
+                      ),
+                      onPressed: () async {
+                        Get.back();
+                      },
+                    ),
+                    TextButton(
+                      child: Text(
+                        '업데이트',
+                        style: TextStyle(
+                          color: AppColors.primary,
+                          fontSize: 17,
+                        ),
+                      ),
+                      onPressed: () async {
+                        if (Platform.isIOS) {
+                          launchUrl(Uri.parse(appStoreUrl));
+                        } else {
+                          launchUrl(Uri.parse(playStoreUrl));
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          backgroundColor: AppColors.boxColor,
+        ),
+      );
+    }
+    if (versionResponse.needUpdate == "FORCE") {
       return Get.dialog(
         AlertDialog(
           title: Text(
