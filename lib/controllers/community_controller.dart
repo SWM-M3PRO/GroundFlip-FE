e620e59ae8491fee0764619ce2b42460e2832249
Koo Min <koomin1227@naver.com>
2024-09-05 21:27:02 +0900

    M3-334 Feat : 그룹 정보 로딩하는 기능 구현

--- lib/controllers/community_controller.dart
+++ lib/controllers/community_controller.dart
@@ -1,19 +1,22 @@
 import 'package:get/get.dart';
 
+import '../models/community.dart';
 import '../models/ranking.dart';
+import '../service/community_service.dart';
 
 class CommunityController extends GetxController {
+  final CommunityService communityService = CommunityService();
   final RxString name = "".obs;
   final RxString imageUrl = "".obs;
-
   final RxInt memberCount = 0.obs;
   final RxInt communityColor = 0.obs;
   final RxInt communityRanking = 0.obs;
   final RxInt currentPixelCount = 0.obs;
   final RxInt accumulatePixelCount = 0.obs;
   final RxInt maxPixelCount = 0.obs;
-  final RxInt maxRankingCount = 0.obs;
+  final RxInt maxRanking = 0.obs;
   final RxBool isJoin = true.obs;
+  final RxBool isLoading = true.obs;
 
   final RxList members = [
     Ranking(userId: 1, rank: 1, currentPixelCount: 123, nickname: "test1"),
@@ -23,16 +26,17 @@ class CommunityController extends GetxController {
     Ranking(userId: 1, rank: 5, currentPixelCount: 123, nickname: "test5"),
   ].obs;
 
-  init(int groupId) {
-    name.value = "세종대학교";
-    imageUrl.value =
-        "https://ground-flip-prod-storage-resized.s3.ap-northeast-2.amazonaws.com/resized-static/42166cf0-b557-42a1-a1fb-d04472ea0ac1%23%23%23622.jpg";
-    memberCount.value = 3;
-    communityColor.value = 0xFF0DF69E;
-    communityRanking.value = 534;
-    currentPixelCount.value = 124;
-    accumulatePixelCount.value = 11394;
-    maxPixelCount.value = 945;
-    maxRankingCount.value = 53;
+  init(int communityId) async {
+    Community community = await communityService.getCommunityInfo(communityId);
+    name.value = community.name;
+    imageUrl.value = community.backgroundImageUrl;
+    memberCount.value = community.memberCount;
+    communityColor.value = community.communityColor;
+    communityRanking.value = community.communityRanking;
+    currentPixelCount.value = community.currentPixelCount;
+    accumulatePixelCount.value = community.accumulatePixelCount;
+    maxPixelCount.value = community.maxPixelCount;
+    maxRanking.value = community.maxRanking;
+    isLoading.value = false;
   }
 }
