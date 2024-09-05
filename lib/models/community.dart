f482fc8cb5f7596867e68ebdd86300c147dd1030
Koo Min <koomin1227@naver.com>
2024-09-05 20:18:13 +0900

    M3-334 Feat : Community 구현

---
+++ lib/models/community.dart
@@ -0,0 +1,39 @@
+class Community {
+  int communityRanking;
+  String name;
+  int communityColor;
+  String backgroundImageUrl;
+  int memberCount;
+  int currentPixelCount;
+  int accumulatePixelCount;
+  int maxPixelCount;
+  int maxRanking;
+
+  Community({
+    required this.communityRanking,
+    required this.name,
+    required this.communityColor,
+    required this.backgroundImageUrl,
+    required this.memberCount,
+    required this.currentPixelCount,
+    required this.accumulatePixelCount,
+    required this.maxPixelCount,
+    required this.maxRanking,
+  });
+
+  factory Community.fromJson(Map<String, dynamic> json) {
+    return Community(
+      communityRanking: json['communityRanking'],
+      name: json['name'],
+      communityColor: json['communityColor'] is String
+          ? int.parse(json['communityColor'], radix: 16)
+          : json['communityColor'],
+      backgroundImageUrl: json['backgroundImageUrl'],
+      memberCount: json['memberCount'],
+      currentPixelCount: json['currentPixelCount'],
+      accumulatePixelCount: json['accumulatePixelCount'],
+      maxPixelCount: json['maxPixelCount'],
+      maxRanking: json['maxRanking'],
+    );
+  }
+}
