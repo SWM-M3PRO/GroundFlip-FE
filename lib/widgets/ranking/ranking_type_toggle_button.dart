fe08987b4a84423cd5a8b18cca1f045afcd006da
Koo Min <koomin1227@naver.com>
2024-09-11 21:51:25 +0900

    M3-339 Feat : RankingTypeToggleButton 가운데 정렬

--- lib/widgets/ranking/ranking_type_toggle_button.dart
+++ lib/widgets/ranking/ranking_type_toggle_button.dart
@@ -12,51 +12,53 @@ class RankingTypeToggleButton extends StatelessWidget {
   Widget build(BuildContext context) {
     final RankingController rankingController = Get.find<RankingController>();
     return Obx(() {
-      return AnimatedToggleSwitch<int>.size(
-        current: rankingController.getSelectedType(),
-        style: ToggleStyle(
-          backgroundColor: AppColors.backgroundSecondary,
-          indicatorColor: AppColors.buttonColor,
-          borderColor: Colors.transparent,
-          borderRadius: BorderRadius.circular(24.0),
-          indicatorBorderRadius: BorderRadius.circular(24.0),
-        ),
-        values: const [0, 1],
-        iconOpacity: 1.0,
-        selectedIconScale: 1.0,
-        indicatorSize: Size.fromWidth(175),
-        height: 48,
-        iconAnimationType: AnimationType.onHover,
-        styleAnimationType: AnimationType.onHover,
-        spacing: 2.0,
-        customSeparatorBuilder: (context, local, global) {
-          final opacity =
-              ((global.position - local.position).abs() - 0.5).clamp(0.0, 1.0);
-          return VerticalDivider(
-            indent: 10.0,
-            endIndent: 10.0,
-            color: Colors.white38.withOpacity(opacity),
-          );
-        },
-        customIconBuilder: (context, local, global) {
-          final text = const ['개인', '그룹'][local.index];
-          return Center(
-            child: Text(
-              text,
-              style: TextStyle(
-                color: Color.lerp(
-                  Colors.white,
-                  Colors.black,
-                  local.animationValue,
+      return Center(
+        child: AnimatedToggleSwitch<int>.size(
+          current: rankingController.getSelectedType(),
+          style: ToggleStyle(
+            backgroundColor: AppColors.backgroundSecondary,
+            indicatorColor: AppColors.buttonColor,
+            borderColor: Colors.transparent,
+            borderRadius: BorderRadius.circular(24.0),
+            indicatorBorderRadius: BorderRadius.circular(24.0),
+          ),
+          values: const [0, 1],
+          iconOpacity: 1.0,
+          selectedIconScale: 1.0,
+          indicatorSize: Size.fromWidth(175),
+          height: 48,
+          iconAnimationType: AnimationType.onHover,
+          styleAnimationType: AnimationType.onHover,
+          spacing: 2.0,
+          customSeparatorBuilder: (context, local, global) {
+            final opacity = ((global.position - local.position).abs() - 0.5)
+                .clamp(0.0, 1.0);
+            return VerticalDivider(
+              indent: 10.0,
+              endIndent: 10.0,
+              color: Colors.white38.withOpacity(opacity),
+            );
+          },
+          customIconBuilder: (context, local, global) {
+            final text = const ['개인', '그룹'][local.index];
+            return Center(
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: Color.lerp(
+                    Colors.white,
+                    Colors.black,
+                    local.animationValue,
+                  ),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
                 ),
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
               ),
-            ),
-          );
-        },
-        borderWidth: 0.0,
-        onChanged: (i) => rankingController.updateSelectedType(i),
+            );
+          },
+          borderWidth: 0.0,
+          onChanged: (i) => rankingController.updateSelectedType(i),
+        ),
       );
     });
   }
