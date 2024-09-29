70702e8047e5df1f5890455554f2e028a1e14f76
MinUk <qjvk2880@gmail.com>
2024-09-29 20:34:55 +0900

    M3-379 Refactor : dart fix

--- lib/utils/android_notification.dart
+++ lib/utils/android_notification.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:isolate';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:pedometer/pedometer.dart';
