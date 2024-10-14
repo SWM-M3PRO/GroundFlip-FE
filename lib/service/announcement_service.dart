4f8e454a6e8e798c2599aa8cc87c5d4693d4ef60
Koo Min <koomin1227@naver.com>
2024-10-15 01:08:44 +0900

    M3-407 Feat : 공지사항 세부사항을 조회하는 메서드 구현

--- lib/service/announcement_service.dart
+++ lib/service/announcement_service.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 
 import '../models/announcement.dart';
+import '../models/announcement_info.dart';
 import '../models/event.dart';
 import '../utils/dio_service.dart';
 
@@ -28,5 +29,12 @@ class AnnouncementService {
       },
     );
     return Announcement.listFromJson(response.data['data']);
+  }
+
+  Future<AnnouncementInfo> getAnnouncementContent(int announcementId) async {
+    var response = await dio.get(
+      '/announcement/$announcementId',
+    );
+    return AnnouncementInfo.fromJson(response.data['data']);
   }
 }
