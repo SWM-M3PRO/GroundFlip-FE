adb2043c86e012f69d684e7faeef90529d4ab799
cha-imag <100341870+tkdals802@users.noreply.github.com>
2024-10-16 21:25:38 +0900

    M3-404 Feat : dart fix

--- lib/models/create_community_response.dart
+++ lib/models/create_community_response.dart
@@ -2,11 +2,13 @@ class CreateCommunityResponse {
   late int communityId;
   late int statusCode;
 
-  CreateCommunityResponse(
-      {required this.communityId, required this.statusCode});
+  CreateCommunityResponse({
+    required this.communityId,
+    required this.statusCode,
+  });
 
   CreateCommunityResponse.fromJson(Map<String, dynamic> json, int statusCode) {
-    this.communityId = json['data'];  // 'data' 필드에서 communityId 추출
-    this.statusCode = statusCode;  // 응답의 statusCode 저장
+    communityId = json['data'];
+    statusCode = statusCode;
   }
 }
