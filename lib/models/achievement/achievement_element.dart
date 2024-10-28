4c757ea988d10d7d49f67115a0c9d91e3c87061e
Koo Min <koomin1227@naver.com>
2024-10-28 23:57:11 +0900

    M3-414 Feat : achievement 조회 모델 구현

---
+++ lib/models/achievement/achievement_element.dart
@@ -0,0 +1,31 @@
+class AchievementElement {
+  final int achievementId;
+  final String achievementName;
+  final String badgeImageUrl;
+  final DateTime? obtainedDate;
+  final int categoryId;
+
+  AchievementElement({
+    required this.achievementId,
+    required this.achievementName,
+    required this.badgeImageUrl,
+    required this.obtainedDate,
+    required this.categoryId,
+  });
+
+  factory AchievementElement.fromJson(Map<String, dynamic> json) {
+    return AchievementElement(
+      achievementId: json['achievementId'] as int,
+      achievementName: json['achievementName'] as String,
+      badgeImageUrl: json['badgeImageUrl'] as String,
+      obtainedDate: DateTime.parse(json['obtainedDate'] as String),
+      categoryId: json['categoryId'] as int,
+    );
+  }
+
+  static List<AchievementElement> listFromJson(List<dynamic> jsonList) {
+    return [
+      for (var element in jsonList) AchievementElement.fromJson(element),
+    ];
+  }
+}
