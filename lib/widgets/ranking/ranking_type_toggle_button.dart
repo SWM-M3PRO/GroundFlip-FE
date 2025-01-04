06e6ac66056f432b74ec02bdc69cd5312694c2c2
Koo Min <koomin1227@naver.com>
2025-01-05 02:08:47 +0900

    M3-422 Feat : 그룹 모드에서는 rankingType 이 선택되지 않도록 수정

--- lib/widgets/ranking/ranking_type_toggle_button.dart
+++ lib/widgets/ranking/ranking_type_toggle_button.dart
@@ -13,55 +13,60 @@ class RankingTypeToggleButton extends StatelessWidget {
   Widget build(BuildContext context) {
     final RankingController rankingController = Get.find<RankingController>();
     return Obx(() {
-      return Center(
-        child: AnimatedToggleSwitch<int>.size(
-          current:
-              rankingController.getRankingType() == RankingType.current ? 0 : 1,
-          style: ToggleStyle(
-            backgroundColor: AppColors.backgroundSecondary,
-            indicatorColor: AppColors.buttonColor,
-            borderColor: Colors.transparent,
-            borderRadius: BorderRadius.circular(24.0),
-            indicatorBorderRadius: BorderRadius.circular(24.0),
-          ),
-          values: const [0, 1],
-          iconOpacity: 1.0,
-          selectedIconScale: 1.0,
-          indicatorSize: Size.fromWidth(50),
-          height: 28,
-          iconAnimationType: AnimationType.onHover,
-          styleAnimationType: AnimationType.onHover,
-          spacing: 2.0,
-          customSeparatorBuilder: (context, local, global) {
-            final opacity = ((global.position - local.position).abs() - 0.5)
-                .clamp(0.0, 1.0);
-            return VerticalDivider(
-              indent: 10.0,
-              endIndent: 10.0,
-              color: Colors.white38.withOpacity(opacity),
-            );
-          },
-          customIconBuilder: (context, local, global) {
-            final text = const ['현재', '누적'][local.index];
-            return Center(
-              child: Text(
-                text,
-                style: TextStyle(
-                  color: Color.lerp(
-                    Colors.white,
-                    Colors.black,
-                    local.animationValue,
+      if (rankingController.getSelectedType() == 0) {
+        return Center(
+          child: AnimatedToggleSwitch<int>.size(
+            current: rankingController.getRankingType() == RankingType.current
+                ? 0
+                : 1,
+            style: ToggleStyle(
+              backgroundColor: AppColors.backgroundSecondary,
+              indicatorColor: AppColors.buttonColor,
+              borderColor: Colors.transparent,
+              borderRadius: BorderRadius.circular(24.0),
+              indicatorBorderRadius: BorderRadius.circular(24.0),
+            ),
+            values: const [0, 1],
+            iconOpacity: 1.0,
+            selectedIconScale: 1.0,
+            indicatorSize: Size.fromWidth(50),
+            height: 28,
+            iconAnimationType: AnimationType.onHover,
+            styleAnimationType: AnimationType.onHover,
+            spacing: 2.0,
+            customSeparatorBuilder: (context, local, global) {
+              final opacity = ((global.position - local.position).abs() - 0.5)
+                  .clamp(0.0, 1.0);
+              return VerticalDivider(
+                indent: 10.0,
+                endIndent: 10.0,
+                color: Colors.white38.withOpacity(opacity),
+              );
+            },
+            customIconBuilder: (context, local, global) {
+              final text = const ['현재', '누적'][local.index];
+              return Center(
+                child: Text(
+                  text,
+                  style: TextStyle(
+                    color: Color.lerp(
+                      Colors.white,
+                      Colors.black,
+                      local.animationValue,
+                    ),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
                   ),
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600,
                 ),
-              ),
-            );
-          },
-          borderWidth: 0.0,
-          onChanged: (i) => rankingController.updateRakingType(i),
-        ),
-      );
+              );
+            },
+            borderWidth: 0.0,
+            onChanged: (i) => rankingController.updateRakingType(i),
+          ),
+        );
+      } else {
+        return SizedBox();
+      }
     });
   }
 }
