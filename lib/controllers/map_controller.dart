0e159120bde6cec2711e79748fad67169f15032d
MinUk <qjvk2880@gmail.com>
2025-01-06 14:03:41 +0900

    M3-424 Feat : 줌 레벨이 변화했거나 threshold를 초과하였을 경우에만 API 요청하도록 최적화

--- lib/controllers/map_controller.dart
+++ lib/controllers/map_controller.dart
@@ -61,6 +61,7 @@ class MapController extends SuperController {
 
   final box = GetStorage();
 
+  late CameraPosition lastStoppedCameraPosition;
   late CameraPosition currentCameraPosition;
   late Map<String, int> latestPixel;
 
@@ -178,14 +179,29 @@ class MapController extends SuperController {
     if (!isBottomSheetShowUp) {
       _cameraIdleTimer = Timer(Duration(milliseconds: 300), () {
         final currentZoomLevel = currentCameraPosition.zoom;
-        final moveAmountThreshold = _calculateThresholdByZoom(currentZoomLevel);
+        final calculatedThreshold = _calculateThresholdByZoom(currentZoomLevel);
+        final zoomLevelChanged = _hasZoomChanged(currentZoomLevel);
 
+        if (zoomLevelChanged || _cameraMovedOverThreshold(calculatedThreshold)) {
+          updateMap();
+        }
 
-        // updateMap();
+        _lastZoomLevel = currentCameraPosition.zoom;
+        lastStoppedCameraPosition = currentCameraPosition;
       });
     }
   }
 
+  bool _cameraMovedOverThreshold(double threshold) {
+    final distance = _calculateDistance(currentCameraPosition.target, lastStoppedCameraPosition.target);
+    return distance > threshold;
+  }
+
+
+  bool _hasZoomChanged(double currentZoom) {
+    if (_lastZoomLevel == null) return true;
+    return (currentZoom - _lastZoomLevel!).abs() > zoomChangeThreshold;
+  }
 
   double _calculateThresholdByZoom(double zoom) {
     const double baseZoomLevel = 16;
@@ -208,6 +224,7 @@ class MapController extends SuperController {
       ),
       zoom: 16.0,
     );
+    lastStoppedCameraPosition = currentCameraPosition;
     googleMapController?.animateCamera(
       CameraUpdate.newCameraPosition(currentCameraPosition),
     );
